74a10f1a049241919c5002e5d686f50b0627a98d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-05-07 23:25:21 +0700

    Add "Simple" primitive data components

---
+++ Scripts/BaseSimplePrimitiveData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseSimplePrimitiveData : MonoBehaviour
+{
+    public Color gizmosColor = Color.magenta;
+    public abstract Vector3 GetRandomPosition();
+}
---
+++ Scripts/SimpleCubeData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCubeData : BaseSimplePrimitiveData
+{
+    public Vector3 size;
+    public bool isFixRandomX;
+    public float fixRandomX;
+    public bool isFixRandomY;
+    public float fixRandomY;
+    public bool isFixRandomZ;
+    public float fixRandomZ;
+
+    protected virtual void OnDrawGizmosSelected()
+    {
+        Gizmos.color = gizmosColor;
+        Gizmos.DrawWireCube(transform.position, size);
+    }
+
+    public override Vector3 GetRandomPosition()
+    {
+        return transform.position + new Vector3(
+            !isFixRandomX ? Random.Range(-size.x * 0.5f, size.x * 0.5f) : fixRandomX,
+            !isFixRandomY ? Random.Range(-size.y * 0.5f, size.y * 0.5f) : fixRandomY,
+            !isFixRandomZ ? Random.Range(-size.z * 0.5f, size.z * 0.5f) : fixRandomZ);
+    }
+}
